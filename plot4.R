bb06e68ccfd1b519085d37014d86d9f0ab7e267d
jaelison <jaelison@hotmail.com>
2015-08-09 07:35:09 -0300

    Updated
    Updated

--- plot4.R
+++ plot4.R
@@ -52,23 +52,24 @@
 # Plot 4
 
 	par(mfrow=c(2,2))
-	hist(epc$Global_active_power, xlab = "Global Active Power (kilowatts)", main="Global Active Power", col="red")
-	plot(epc$DateTime, epc$Voltage, type="o", pch=21, cex=0, xlab="", ylab="Voltage")
-	plot(epc$Sub_metering_1 ~ epc$DateTime , type="o", pch=21, cex=0, xlab="", ylab="")
+	plot(epc$DateTime, epc$Global_active_power, type="o", pch=21, cex=0, xlab="", ylab="Global Active Power")
+	plot(epc$DateTime, epc$Voltage, type="o", pch=21, cex=0, xlab="datetime", ylab="Voltage")
+	plot(epc$Sub_metering_1 ~ epc$DateTime , type="o", pch=21, cex=0, xlab="", ylab="Energy sub metering")
 	points(epc$Sub_metering_2 ~ epc$DateTime , type="o", pch=21, cex=0, xlab="", ylab="", col="red")
 	points(epc$Sub_metering_3 ~ epc$DateTime , type="o", pch=21, cex=0, xlab="", ylab="", col="blue")
 	legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lwd=2, col=c("black","red","blue"))
-	plot(epc$DateTime, epc$Global_reactive_power, type="o", pch=21, cex=0, xlab="", ylab="Global_reactive_power")
+	plot(epc$DateTime, epc$Global_reactive_power, type="o", pch=21, cex=0, xlab="datetime", ylab="Global_reactive_power")
 
 	png(file = "plot4.png",width = 480, height = 480)
 	par(mfrow=c(2,2))
-	hist(epc$Global_active_power, xlab = "Global Active Power (kilowatts)", main="Global Active Power", col="red")
-	plot(epc$DateTime, epc$Voltage, type="o", pch=21, cex=0, xlab="", ylab="Voltage")
-	plot(epc$Sub_metering_1 ~ epc$DateTime , type="o", pch=21, cex=0, xlab="", ylab="")
+	plot(epc$DateTime, epc$Global_active_power, type="o", pch=21, cex=0, xlab="", ylab="Global Active Power")
+	plot(epc$DateTime, epc$Voltage, type="o", pch=21, cex=0, xlab="datetime", ylab="Voltage")
+	plot(epc$Sub_metering_1 ~ epc$DateTime , type="o", pch=21, cex=0, xlab="", ylab="Energy sub metering")
 	points(epc$Sub_metering_2 ~ epc$DateTime , type="o", pch=21, cex=0, xlab="", ylab="", col="red")
 	points(epc$Sub_metering_3 ~ epc$DateTime , type="o", pch=21, cex=0, xlab="", ylab="", col="blue")
 	legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lwd=2, col=c("black","red","blue"))
-	plot(epc$DateTime, epc$Global_reactive_power, type="o", pch=21, cex=0, xlab="", ylab="Global_reactive_power")
+	plot(epc$DateTime, epc$Global_reactive_power, type="o", pch=21, cex=0, xlab="datetime", ylab="Global_reactive_power")
 	dev.off()
+
 
 
